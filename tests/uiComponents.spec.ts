0c8abe8a3da8af5a493928bbfa4ec81a02d3830f
SaltyEightLab <hachiman_hachi@icloud.com>
2024-11-24 11:14:11 +0900

    about radio button

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -23,3 +23,35 @@ test("input fields", async ({ page }) => {
   //locator assertion
   await expect(usingTheGridEmailInput).toHaveValue("test@test.com");
 });
+
+test("radio buttons", async ({ page }) => {
+  await page.getByText("Forms").click();
+  await page.getByText("Form Layouts").click();
+  const usingTheGridForm = page.locator("nb-card", {
+    hasText: "Using the Grid",
+  });
+
+  // await usingTheGridForm.getByLabel("Option 1").check({force: true});
+  await usingTheGridForm
+    .getByRole("radio", { name: "Option 1" })
+    .check({ force: true });
+  const radioStatus = await usingTheGridForm
+    .getByRole("radio", {
+      name: "Option 1",
+    })
+    .isChecked();
+  expect(radioStatus).toBeTruthy();
+  await expect(
+    usingTheGridForm.getByRole("radio", { name: "Option 1" })
+  ).toBeChecked();
+
+  await usingTheGridForm.getByRole("radio", { name: "Option 2" }).check({
+    force: true,
+  });
+  expect(
+    await usingTheGridForm.getByRole("radio", { name: "Option 1" }).isChecked()
+  ).toBeFalsy();
+  expect(
+    await usingTheGridForm.getByRole("radio", { name: "Option 2" }).isChecked()
+  ).toBeTruthy();
+});
