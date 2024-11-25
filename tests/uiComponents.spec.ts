019527a8efee6a19aceb35bdaad0a4aadd746c87
SaltyEightLab <hachiman_hachi@icloud.com>
2024-11-25 15:11:47 +0900

    about dialog test

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -102,3 +102,37 @@ test("lists and dropdowns", async ({ page }) => {
     await dropDownMenu.click();
   }
 });
+
+test("tooltips", async ({ page }) => {
+  await page.getByText("Modal & Overlays").click();
+  await page.getByText("Tooltip").click();
+
+  const toolTipCard = page.locator("nb-card", {
+    hasText: "Tooltip Placements",
+  });
+  await toolTipCard.getByRole("button", { name: "Top" }).hover();
+
+  page.getByRole("tooltip"); //if you have a role tooltip created
+  const tooltip = await page.locator("nb-tooltip").textContent();
+  expect(tooltip).toEqual("This is a tooltip");
+});
+
+test("dialog box", async ({ page }) => {
+  await page.getByText("Tables & Data").click();
+  await page.getByText("Smart Table").click();
+
+  page.on("dialog", (dialog) => {
+    expect(dialog.message()).toEqual("Are you sure you want to delete?");
+    dialog.accept();
+  });
+
+  const trashIcon = page
+    .getByRole("table")
+    .locator("tr", { hasText: "mdo@gmail.com" })
+    .locator(".nb-trash");
+
+  await trashIcon.click();
+  await expect(page.locator("table tr").first()).not.toHaveText(
+    "mdo@gmail.com"
+  );
+});
